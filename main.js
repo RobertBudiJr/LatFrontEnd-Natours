e5604143d75b59087961465f5e9d6edd79e122a5
Valenta Abram Nugraha Putra <72144055+RobertBudiJr@users.noreply.github.com>
2022-06-13 14:05:45 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const options = {
+  threshold: 0.5,
+};
+
+const addActiveClass = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && entry.intersectionRatio >= 0.1) {
+      // console.log(entry.target);
+      let currentActive = document.querySelector('.desktop-nav a.active');
+
+      if (currentActive) {
+        currentActive.classList.remove('active');
+      }
+
+      let newActive = document.querySelector(`.desktop-nav a[href="#${entry.target.getAttribute('id')}"]`);
+
+      newActive.classList.add('active');
+    }
+  });
+};
+
+const observer = new IntersectionObserver(addActiveClass, options);
+
+const sections = document.querySelectorAll('section');
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
